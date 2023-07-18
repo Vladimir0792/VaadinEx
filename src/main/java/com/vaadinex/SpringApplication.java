c5f196ea7cceed2f7650eb55d0215e98da57ec0a
Vladimir0792 <vova19927@mail.ru>
2023-07-18 18:45:47 +0300

    localhost

---
+++ src/main/java/com/vaadinex/SpringApplication.java
@@ -0,0 +1,13 @@
+package com.vaadinex;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplication.class, args);
+    }
+
+    private static void run(Class<SpringApplication> springApplicationClass, String[] args) {
+    }
+}
